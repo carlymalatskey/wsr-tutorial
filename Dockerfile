e203f40a539a3a179dae8b77b544c74b26090ea4
carlymalatskey <carlym@wix.com>
2021-03-21 16:52:54 +0200

    wsr tutorial set-up

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM docker-repo.wixpress.com/wix-bootstrap-onbuild:stable
+MAINTAINER carlymalatskey <carlym@wix.com>
---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'wix-style-react';
+
+export default () => (
+  <Button>
+    Hello world
+  </Button>
+);
